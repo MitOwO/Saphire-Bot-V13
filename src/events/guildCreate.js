d0b848441d5a1b486aef6c0b402af706a555c0dd
Rodrigo Couto <70030588+rodycouto@users.noreply.github.com>
2021-09-14 03:46:18 -0300

    Update guildCreate.js

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,76 +1,32 @@
 const client = require('../../index')
-const { MessageEmbed } = require('discord.js')
-const { e } = require('../../Routes/emojis.json')
-const db = require('quick.db')
-const { config } = require('../../Routes/config.json')
-
-client.on("guildCreate", async (guild) => {
-
-    if (!guild.available) return
-    db.set(`Servers.${guild.id}`, guild.name)
-
-    let owner = await guild.fetchOwner()
-
-    const Embed = new MessageEmbed()
-        .setColor('GREEN')
-        .setTitle(`${e.Loud} Um servidor me adicionou`)
-        .setDescription('Registro no banco de dados com concluido!')
-        .addField('Servidor', `${guild.name} *\`(${guild.id})\`*`)
-        .addField('Status', `**Dono:** ${owner.user.tag} *\`(${owner.user.id})\`*\n**Membros:** ${guild.memberCount}`)
-
-    let ChannelId = config.guildCreateChannelId
-    if (!ChannelId || ChannelId === null || ChannelId === undefined) {
-        return
-    } else {
-        const channel = client.channels.cache.get(ChannelId)
-        if (!channel) { return } else { channel.send({ embeds: [Embed] }) }
-    }
-})const client = require('../../index')
-
 const { MessageEmbed, Permissions } = require('discord.js')
-
 const { e } = require('../../Routes/emojis.json')
-
 const db = require('quick.db')
-
 const { config } = require('../../Routes/config.json')
 
 client.on("guildCreate", async (guild) => {
 
     if (!guild.available) return
-
     db.set(`Servers.${guild.id}`, guild.name)
 
     let owner = await guild.fetchOwner()
-
-    let ChannelInvite = await guild.channels.cache.find(channel => channel.type === 'GUILD_TEXT' && channel.permissionsFor(guild.me).has(Permissions.FLAGS.SEND_MESSAGES))
-
+    let CanalDeConvite = await guild.channels.cache.find(channel => channel.type === 'GUILD_TEXT' && channel.permissionsFor(guild.me).has(Permissions.FLAGS.SEND_MESSAGES))
     let ChannelId = config.guildCreateChannelId
 
     function WithChannel() {
-
-        ChannelInvite.createInvite({ maxAge: 0 }).then(ChannelInvite => {
-
+        CanalDeConvite.createInvite({ maxAge: 0 }).then(ChannelInvite => {
             const Embed = new MessageEmbed().setColor('GREEN').setTitle(`${e.Loud} Um servidor me adicionou`).setDescription('Registro no banco de dados com concluido!').addField('Servidor', `[${guild.name}](${ChannelInvite.url}) *\`(${guild.id})\`*`).addField('Status', `**Dono:** ${owner.user.tag} *\`(${owner.user.id})\`*\n**Membros:** ${guild.memberCount}`)
-
             if (!ChannelId) { return } else { const channel = client.channels.cache.get(ChannelId); if (!channel) { return } else { channel.send({ embeds: [Embed] }) } }
-
         }).catch(() => {
-
             const Embed = new MessageEmbed().setColor('GREEN').setTitle(`${e.Loud} Um servidor me adicionou`).setDescription('Registro no banco de dados com concluido!').addField('Servidor', `${guild.name} *\`(${guild.id})\`*`).addField('Status', `**Dono:** ${owner.user.tag} *\`(${owner.user.id})\`*\n**Membros:** ${guild.memberCount}`)
-
             if (!ChannelId) { return } else { const channel = client.channels.cache.get(ChannelId); if (!channel) { return } else { channel.send({ embeds: [Embed] }) } }
-
         })
-
     }
 
     function WithoutChannel() {
-
         const Embed = new MessageEmbed().setColor('GREEN').setTitle(`${e.Loud} Um servidor me adicionou`).setDescription('Registro no banco de dados com concluido!').addField('Servidor', `${guild.name} *\`(${guild.id})\`*`).addField('Status', `**Dono:** ${owner.user.tag} *\`(${owner.user.id})\`*\n**Membros:** ${guild.memberCount}`)
         if (!ChannelId) { return } else { const channel = client.channels.cache.get(ChannelId); if (!channel) { return } else { channel.send({ embeds: [Embed] }) } }
-
     }
 
-    ChannelInvite ? WithChannel() : WithoutChannel()
+    CanalDeConvite ? WithChannel() : WithoutChannel()
 })
