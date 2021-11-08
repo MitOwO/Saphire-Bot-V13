1a457770fb869ea3d3f515954810da7a74cd3008
Rodrigo Couto <70030588+rodycouto@users.noreply.github.com>
2021-11-07 23:48:07 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,6 @@
-const { Collection, Client, MessageEmbed } = require('discord.js')
-const fs = require('fs');
-const { e } = require('./Routes/emojis.json')
-const { config } = require('./Routes/config.json')
-require("dotenv").config()
-const client = new Client({ intents: 32767, disableMentions: { parse: ['everyone'], partials: ['MESSAGE', 'CHANNEL', 'REACTION'] } })
+const SaphireClient = require("./Routes/classes/SaphireClient")
+const client = new SaphireClient()
 module.exports = client
-client.commands = new Collection()
-client.aliases = new Collection()
-client.categories = fs.readdirSync("./src/commands/")
-
-process.on('unhandledRejection', (reason) => {
-    if (reason.code === 500)
-        return client.users.cache.get(`${config.ownerId}`)?.send({ content: 'unhandledRejection - GOT! Error.code 500!!!' }).catch(() => { })
-
-    if (reason.code === 50035) return // Invalid Form Body
-    if (reason.code === 10008) return // Unknown Message
-    if (reason.code === 50001) return // Missing Acess
-    if (reason.code === 50013) return // Missing Permissions
-    const NewError = new MessageEmbed().setColor('RED').setTitle(`${e.Loud} Report de Erro | unhandledRejection`).setDescription(`\`\`\`js\n${reason.stack.slice(0, 2000)}\`\`\``)
-    client.users.cache.get(`${config.ownerId}`)?.send({ embeds: [NewError] }).catch(() => { })
-});
-
-process.on('uncaughtExceptionMonitor', async (error, origin) => {
-    if (error.code === 10008) return // Unknown Message
-    if (error.code === 50001) return // Missing Acess
-    if (error.code === 50013) return // Missing Permissions
-    const NewError = new MessageEmbed().setColor('RED').setTitle(`${e.Loud} Report de Erro | uncaughtExceptionMonitor`).setDescription(`\`\`\`js\n${error.stack.slice(0, 2000)}\`\`\``)
-    const NewError2 = new MessageEmbed().setColor('RED').setDescription(`\`\`\`js\n${origin}\`\`\``)
-    client.users.cache.get(`${config.ownerId}`)?.send({ embeds: [NewError, NewError2] }).catch(() => { })
-});
-
-["command", "event"].forEach(structure => { require(`./src/structures/${structure}`)(client); })
-client.login(process.env.DISCORD_CLIENT_BOT_TOKEN)
+require('./Routes/functions/process')
+for (const file of ["command", "event"]) { require(`./src/structures/${file}`)(client) }
+client.start()
--- shard.js
+++ shard.js
@@ -1,7 +1,7 @@
 const { ShardingManager } = require('discord.js')
-const client = require('./index.js')
-const { config } = require('./Routes/config.json')
-const { e } = require('./Routes/emojis.json')
+const { DatabaseObj } = require('./Routes/functions/database')
+const { e, config } = DatabaseObj
+const client = require('./index')
 const Data = require('./Routes/functions/data')
 require("dotenv").config()
 
@@ -11,24 +11,26 @@ let manager = new ShardingManager('./index.js', {
     totalShards: 'auto'
 })
 
+const channel = async () => { await client.channels.cache.get(config.LogChannelId) }
+
 manager.on('shardCreate', async (shard) => {
-    client.channels.cache.get(config.LogChannelId).send(`${e.Check} | Um novo Shard foi criado.\n${e.Info} | Shard: ${shard.id} \`${Data}\``)
+    channel.send(`${e.Check} | Um novo Shard foi criado.\n${e.Info} | Shard: ${shard.id} \`${Data()}\``)
 })
 
-manager.on('shardDisconnect', async (shard) => {
-    client.channels.cache.get(config.LogChannelId).send(`${e.Deny} | Um Shard foi desconectado.\n${e.Info} | Shard: ${shard.id} \`${Data}\``)
-})
+    .on('shardDisconnect', async (shard) => {
+        channel.send(`${e.Deny} | Um Shard foi desconectado.\n${e.Info} | Shard: ${shard.id} \`${Data()}\``)
+    })
 
-manager.on('shardError', async (err, shard) => {
-    client.channels.cache.get(config.LogChannelId).send(`${e.Warn} | Ocorreu um erro em um Shard.\n${e.Info} | Shard: ${shard.id} \`${Data}\`\n-> \`${err}\``)
-})
+    .on('shardError', async (err, shard) => {
+        channel.send(`${e.Warn} | Ocorreu um erro em um Shard.\n${e.Info} | Shard: ${shard.id} \`${Data()}\`\n-> \`${err}\``)
+    })
 
-manager.on('shardReady', async (shard) => {
-    client.channels.cache.get(config.LogChannelId).send(`${e.Check} | Shard pronto.\n${e.Info} | Shard: ${shard.id} \`${Data}\``)
-})
+    .on('shardReady', async (shard) => {
+        channel.end(`${e.Check} | Shard pronto.\n${e.Info} | Shard: ${shard.id} \`${Data()}\``)
+    })
 
-manager.on('shardReconnecting', async (shard) => {
-    client.channels.cache.get(config.LogChannelId).send(`${e.Loading} | Shard reconectando.\n${e.Info} | Shard: ${shard.id} \`${Data}\``)
-})
+    .on('shardReconnecting', async (shard) => {
+        channel.send(`${e.Loading} | Shard reconectando.\n${e.Info} | Shard: ${shard.id} \`${Data()}\``)
+    })
 
 manager.spawn()
