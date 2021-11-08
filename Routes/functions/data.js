98bd399045c662f41ff0e12ca2d1ef46260d6a7c
Rodrigo Couto <70030588+rodycouto@users.noreply.github.com>
2021-11-07 23:47:39 -0300

    Delete Routes directory

--- Routes/functions/data.js
+++
@@ -1,2 +0,0 @@
-const Data = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' })
-module.exports = Data
--- Routes/functions/database.js
+++
@@ -1,4 +0,0 @@
-const { Database } = require('simpl.db')
-const sdb = new Database()
-
-module.exports = sdb
