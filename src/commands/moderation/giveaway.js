a62352d5fd06262245eb4c5d35afe525f9dfa912
Rodrigo Couto <70030588+rodycouto@users.noreply.github.com>
2021-11-26 10:56:49 -0300

    Add files via upload

--- src/commands/moderation/giveaway.js
+++ src/commands/moderation/giveaway.js
@@ -15,55 +15,7 @@ module.exports = {
 
     run: async (client, message, args, prefix, db, MessageEmbed, request, sdb) => {
 
-
         return message.reply(`${e.Loading} | Em construção`)
-
-        // const GwManager = new Super.GiveawayManager(message.guild)
-
-        // const { ms, parsems, NewGiveAwayCode, SorteiosAtivados, Emojis } = {
-        //     ms: Super.ms,
-        //     parsems: Super.parsems,
-        //     NewGiveAwayCode: PassCode(7),
-        //     SorteiosAtivados: GwManager.openGiveaways(),
-        //     Emojis: ['✅', '❌'],
-        //     GwChannel: GwManager.Channel(),
-        //     Tada: e.Tada
-        // }
-
-        // if (!GwChannel) return message.reply(`${e.Info} | Não sabe usar o comando de sorteio? Use  \`${prefix}giveaway info\`.`)
-
-        // if (['setchannel'].includes(args[0]?.toLowerCase())) return SetGiveawayChannel()
-        // if (['new', 'create', 'novo'].includes(args[0]?.toLowerCase())) return NewGiveaway()
-
-        // function NewGiveaway() {
-
-        //     const time = args[1]
-        //     const winners = args[2]
-        //     const description = args.slice(3).join(' ')
-
-        //     const WrongParams = `${e.Info} | Para criar um novo sorteio, use esses parâmetros: \`${prefix}gw [tempo] [vencedores] [descrição do sorteio/prêmio]\`\nExemplo: \`${prefix}gw 2h 2 Vaga na Staff\` -> 2 horas, 2 vencedores, vaga na staff`
-
-        //     if (!time) return message.reply(`${e.Deny} Sem tempo definido\n \n${WrongParams}`)
-        //     if (!winners) return message.reply(`${e.Deny} Sem vencedores definido\n \n${WrongParams}`)
-        //     if (!description) return message.reply(`${e.Deny} Sem descrição definida\n \n${WrongParams}`)
-
-        //     const Time, Winners, Description
-
-
-        //     if (!['m', 'h', 'd'].includes(time.slice(-1)))
-        //         return message.reply(`${e.Deny} | Tempo inválido! Verifique se o tempo termina com **"m, h, d"** *(minutos, horas, dias)*`)
-
-        // }
-
-        // function SetGiveawayChannel() {
-        //     const Canal = message.mentions.channels.first()
-
-        //     if (!Canal)
-        //         return message.reply(`${e.Info} | Mencione um #canal para que eu possa configura-lo como um canal de sorteio.`)
-
-        //     ServerDb.set(`Servers.${message.guild.id}.GiveawayChannel`, Canal.id)
-        //     return message.channel.send(`${e.Check} | O canal ${Canal} foi configurado como canal de sorteio. Agora, todos os sorteios serão enviados neste canal.`)
-        // }
 
     }
 }
