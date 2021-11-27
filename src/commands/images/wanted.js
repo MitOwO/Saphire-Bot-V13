32a724959f80c3353853eabbbf9f9c1fda889495
Rodrigo Couto <70030588+rodycouto@users.noreply.github.com>
2021-11-27 09:27:00 -0300

    Add files via upload

--- src/commands/images/wanted.js
+++ src/commands/images/wanted.js
@@ -1,30 +1,24 @@
 const { MessageAttachment } = require('discord.js')
-const { e } = require('../../../database/emojis.json')
+const { DatabaseObj: { e } } = require('../../../Routes/functions/database')
 const { Canvas } = require('canvacord')
 const Error = require('../../../Routes/functions/errors')
 
-// #246FE0 - Azul Saphire
 module.exports = {
     name: 'wanted',
     aliases: ['procurado'],
     category: 'images',
     ClientPermissions: ['ATTACH_FILES'],
-    emoji: `${e.Cadeia}`,
+    emoji: `${e.PepePreso}`,
     usage: '<wanted> [@user]',
     description: 'Wanted meme',
 
     run: async (client, message, args, prefix, db, MessageEmbed, request, sdb) => {
 
-        let user = message.mentions.users.first() || message.mentions.repliedUser || await client.users.cache.get(args[0]) || message.author
-        let avatar = user.displayAvatarURL({ format: 'png' })
+        let user = message.mentions.users.first() || message.mentions.repliedUser || await client.users.cache.get(args[0]) || message.author,
+            avatar = user.displayAvatarURL({ format: 'png' }),
+            msg = await message.reply(`${e.Loading} | Carregando...`)
 
-        try {
-            return message.reply(`${e.Loading} | Carregando...`).then(async msg => {
-                msg.delete().catch(() => { })
-                message.reply({ files: [new MessageAttachment(await Canvas.wanted(avatar), 'wanted.png')] })
-            })
-        } catch (err) {
-            Error(message, err)
-        }
+        message.reply({ files: [new MessageAttachment(await Canvas.wanted(avatar), 'wanted.png')] }).catch(err => Error(message, err))
+        return msg.delete().catch(() => { })
     }
 }
