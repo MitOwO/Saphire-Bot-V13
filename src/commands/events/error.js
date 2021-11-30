2a8ff7f80753b174e12a7adf8a0b24df8f75a873
Rodrigo Couto <70030588+rodycouto@users.noreply.github.com>
2021-11-29 21:16:19 -0300

    Delete src/commands directory

--- src/commands/events/error.js
+++
@@ -1,9 +0,0 @@
-const { DatabaseObj } = require('../../Routes/functions/database')
-const { e, config } = DatabaseObj
-const { MessageEmbed } = require('discord.js')
-const client = require('../../index')
-
-client.on('error', async (error) => {
-    const NewError = new MessageEmbed().setColor('RED').setTitle(`${e.Loud} Report de Erro - CLIENT ERROR`).addField('Aviso', `${error}`)
-    await client.users.cache.get(config.ownerId).send({ embeds: [NewError] }).catch(() => { })
-})
--- src/commands/events/warn.js
+++
@@ -1,9 +0,0 @@
-const { DatabaseObj } = require('../../Routes/functions/database')
-const { e, config } = DatabaseObj
-const { MessageEmbed } = require('discord.js')
-const client = require('../../index')
-
-client.on('warn', async (warn) => {
-    const NewWarn = new MessageEmbed().setColor('RED').setTitle(`${e.Loud} Report de WARN - CLIENT WARN`).addField('Aviso', `${warn}`)
-    await client.users.cache.get(config.ownerId).send({ embeds: [NewWarn] }).catch(() => { })
-})
