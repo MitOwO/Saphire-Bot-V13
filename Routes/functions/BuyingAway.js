e0a80a31e8d629405b3307369f254b7f9416f9fd
Rodrigo Couto <70030588+rodycouto@users.noreply.github.com>
2021-12-06 23:10:35 -0300

    Add files via upload

--- Routes/functions/BuyingAway.js
+++ Routes/functions/BuyingAway.js
@@ -162,7 +162,7 @@ function BuyingAway(message, prefix, args, args1) {
 
             TicketsArray.push(message.author.id)
 
-            if (lotery.get('Loteria.Users')?.length + i >= 150) {
+            if (lotery.get('Loteria.Users')?.length + i >= 15000) {
 
                 sdb.subtract(`Users.${message.author.id}.Balance`, i * 10)
                 lotery.add('Loteria.Prize', i * 10)
